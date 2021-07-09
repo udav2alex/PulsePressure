0b0921aa64236a5c2017e08629c88b446960cd53
udav2alex <udav2alex@gmail.com>
2021-07-09 10:51:02 +0300

    Initial commit

---
+++ app/src/main/java/ru/gressor/pulsepressure/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.gressor.pulsepressure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.gressor.pulsepressure.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/gressor/pulsepressure/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.gressor.pulsepressure.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
